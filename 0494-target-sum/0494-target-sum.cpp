e942f414cc438dc37c20c446327a8f88c0822b37
AKASH PATEL <86844027+akashpatelknit@users.noreply.github.com>
2022-11-25 17:49:32 +0530

    Time: 20 ms (76.00%), Space: 9.5 MB (56.66%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int d) {
-          int k=0;
+        int ts=0;for(auto i:nums) ts+=i;
         int n=nums.size();
-    int ts=0;for(auto i:nums) ts+=i;
-    if(ts-d<0 || (ts-d)%2==1) return 0;
-    else k=(ts-d)/2;
-   vector<vector<int>>dp(n,vector<int>(k+1,0));
-    vector<int>pre(k+1,0),curr(k+1,0);
-   if(nums[0]==0){
-       pre[0]=2;
-   }else pre[0]=1;
-    
-    if(nums[0]!=0 && nums[0]<=k)pre[nums[0]]=1;
-    for(int i=1;i<n;i++){
-        for(int tar=0;tar<=k;tar++){
-            int nottake=pre[tar];
-            int take=0;
-            if(nums[i]<=tar) take=pre[tar-nums[i]];
-            curr[tar]=(take+nottake)%(int)(1e9+7);
+        if(ts-d<0 || (ts-d)%2==1) return 0;
+        int tar=(ts-d)/2;
+        // vector<vector<int>>dp(n,vector<int>(tar+1,0)
+         vector<int>pre(tar+1,0),curr(tar+1,0);
+        if(nums[0]==0){
+            pre[0]=2;
+        }else pre[0]=1;
+        if(nums[0]!=0 && tar>=nums[0]) pre[nums[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=tar;j++){
+                int nt=pre[j];
+                int take=0;
+                if(nums[i]<=j) take=pre[j-nums[i]];
+                curr[j]=take+nt;
+            }
+            pre=curr;
         }
-        pre=curr;
-    }
-    return pre[k];
+        return pre[tar];
     }
 };
