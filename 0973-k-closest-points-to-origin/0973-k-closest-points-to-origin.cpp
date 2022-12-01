8329126c0963de5198d61aeac25a62d5b55b35f5
AKASH PATEL <86844027+akashpatelknit@users.noreply.github.com>
2022-12-01 11:30:52 +0530

    Time: 536 ms (46.75%), Space: 71.3 MB (48.31%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        int j=0;
+        for(auto i:points){
+            int d=i[0]*i[0]+i[1]*i[1];
+            q.push({d,{i[0],i[1]}});
+                  
+        }
+        vector<vector<int>>ans; 
+        while(k--){
+           auto it=q.top();
+            q.pop();
+            auto idx=it.second;
+            ans.push_back({idx.first,idx.second});
+        }
+                   return ans;
+    }
+};
