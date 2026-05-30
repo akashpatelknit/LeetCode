a7c7793aec6acd48902dffe87d987993083c3307
AKASH PATEL <86844027+akashpatelknit@users.noreply.github.com>
2026-05-30 13:30:39 +0530

    Time: 2 ms (15.54%), Space: 8.9 MB (37.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        st.push('0');
+        for (auto i : s) {
+            if (i == '(' || i == '{' || i == '[') {
+                st.push(i);
+            } else {
+                char ch = st.top();
+                if (i == ')' && ch == '(' || i == '}' && ch == '{' ||
+                    i == ']' && ch == '[') {
+                    st.pop();
+                } else {
+                    st.push(i);
+                }
+            }
+        }
+        if (st.top() == '0') {
+            return 1;
+        }
+        return 0;
+    }
+};
